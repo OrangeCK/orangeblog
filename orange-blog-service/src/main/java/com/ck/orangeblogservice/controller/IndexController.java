adb183348239f8ee39fa4646a08df3019d12a231
CK <chenkang_457897564@163.com>
2019-05-22 23:14:28 +0800

    2019-5-22 23:13:26

---
+++ orange-blog-service/src/main/java/com/ck/orangeblogservice/controller/IndexController.java
@@ -0,0 +1,40 @@
+package com.ck.orangeblogservice.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.ck.orangeblogdao.pojo.ResultData;
+import com.ck.orangeblogdao.vo.ImageBlogVo;
+import com.ck.orangeblogservice.service.ImageBlogService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Administrator
+ * @date 2019/5/22 0022
+ * Description :
+ */
+@Controller
+@RequestMapping("/index")
+@Api(description = "主页")
+public class IndexController {
+
+    @Autowired
+    private ImageBlogService imageBlogService;
+
+    /**
+     * 分页查询
+     * @return
+     */
+    @RequestMapping(value = "/imagePageList", method = {RequestMethod.POST,RequestMethod.GET})
+    public ModelAndView imagePageList(){
+        ImageBlogVo imageBlogVo = new ImageBlogVo();
+        ResultData resultData = imageBlogService.imagePageList(imageBlogVo, 1, 10);
+        ModelAndView mv = new ModelAndView("lmindex");
+        mv.addObject("rows", ((IPage)resultData.getData()).getRecords());
+        mv.addObject("total", ((IPage)resultData.getData()).getTotal());
+        return mv;
+    }
+}
